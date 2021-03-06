9882c3d15a40cada1e9c2f6c1aaeed66d66f4c92
Simon Norris <snorris@hillcrestgeo.ca>
2021-03-05 18:32:26 -0800

    convert bash to bat, move load of o-d tables to prelim python script

--- 00_create_network.bat
+++ 00_create_network.bat
@@ -1,47 +1,23 @@
-#!/bin/bash
-set -euxo pipefail
-
-# ----------------
-# load roads
-# ----------------
-ogr2ogr \
-  -t_srs EPSG:3005 \
-  -f PostgreSQL "PG:host=localhost port=5434 user=postgres dbname=thb password=None" \
-  -lco OVERWRITE=YES \
-  -lco GEOMETRY_NAME=geom \
-  -nln roads \
-  -dim XY \
-  -nlt LINESTRING \
-  data/01_working.gdb/ \
+REM ----------------
+REM load roads
+REM ----------------
+ogr2ogr ^
+  -t_srs EPSG:3005 ^
+  -f PostgreSQL "PG:host=localhost port=5434 user=postgres dbname=thb password=None" ^
+  -lco OVERWRITE=YES ^
+  -lco GEOMETRY_NAME=geom ^
+  -nln roads ^
+  -dim XY ^
+  -nlt LINESTRING ^
+  data\01_working.gdb ^
   Roadnet_only2_1_1splitn_1
-# add pgrouting required source/target columns
+
+REM add pgrouting required source/target columns
 psql -p 5434 -U postgres thb -c "ALTER TABLE roads ADD COLUMN source integer;"
 psql -p 5434 -U postgres thb -c "ALTER TABLE roads ADD COLUMN target integer;"
 
-# ----------------
-# load destinations (manuall cleaned csv from source arcgis layer)
-# ----------------
-psql -p 5434 -U postgres thb -c "DROP TABLE IF EXISTS destinations"
-psql -p 5434 -U postgres thb -c "CREATE TABLE destinations (destination_id integer primary key, name text, x double precision, y double precision)"
-psql -p 5434 -U postgres thb -c "\copy destinations FROM 'data/destinations.csv' delimiter ',' csv header"
-psql -p 5434 -U postgres thb -c "ALTER TABLE destinations ADD COLUMN geom geometry(Point, 3005)"
-psql -p 5434 -U postgres thb -c "UPDATE destinations SET geom = ST_Transform(ST_SetSRID(ST_Point(x, y), 4326), 3005)"
-psql -p 5434 -U postgres thb -c "ALTER TABLE destinations DROP COLUMN x"
-psql -p 5434 -U postgres thb -c "ALTER TABLE destinations DROP COLUMN y"
 
-# ----------------
-# load origins (manually cleaned csv taken from source csv)
-# ----------------
-# rather than messing with ogr2ogr syntax, load origins coordinates directly and convert to geom in the db
-psql -p 5434 -U postgres thb -c "DROP TABLE IF EXISTS origins"
-psql -p 5434 -U postgres thb -c "CREATE TABLE origins (origin_id integer primary key, x double precision, y double precision)"
-psql -p 5434 -U postgres thb -c "\copy origins FROM 'data/origins.csv' delimiter ',' csv header"
-psql -p 5434 -U postgres thb -c "ALTER TABLE origins ADD COLUMN geom geometry(Point, 3005)"
-psql -p 5434 -U postgres thb -c "UPDATE origins SET geom = ST_Transform(ST_SetSRID(ST_Point(x, y), 4326), 3005)"
-psql -p 5434 -U postgres thb -c "ALTER TABLE origins DROP COLUMN x"
-psql -p 5434 -U postgres thb -c "ALTER TABLE origins DROP COLUMN y"
-
-# ----------------
-# build the network topology - this takes about 11min on my machine
-# ----------------
-time psql -p 5434 -U postgres thb -c "SELECT pgr_createTopology('roads', 0.000001, 'geom', 'objectid');"
+REM ----------------
+REM build the network topology - this takes about 11min on my machine
+REM ----------------
+psql -p 5434 -U postgres thb -c "SELECT pgr_createTopology('roads', 0.000001, 'geom', 'objectid');"
--- setup.bat
+++ setup.bat
@@ -1,26 +1,23 @@
-#!/bin/bash
-set -euxo pipefail
-
-# ----------------
-# install postgres/postgis/pgrouting
-# ----------------
+REM ----------------
+REM install postgres/postgis/pgrouting
+REM ----------------
 docker pull pgrouting/pgrouting:13-3.1-3.1.3
-docker run -d \
-  -p 5434:5432 \
-  --name=pgrouting \
-  -e POSTGRES_HOST_AUTH_METHOD=trust \
+docker run -d ^
+  -p 5434:5432 ^
+  --name=pgrouting ^
+  -e POSTGRES_HOST_AUTH_METHOD=trust ^
   pgrouting/pgrouting:13-3.1-3.1.3
 
-# ----------------
-# setup the database
-# ----------------
+REM ----------------
+REM setup the database
+REM ----------------
 psql -p 5434 -U postgres postgres -c "CREATE DATABASE tbh"
 psql -p 5434 -U postgres thb -c "CREATE extension postgis;"
 psql -p 5434 -U postgres thb -c "CREATE extension pgrouting;"
 
-# ----------------
-# setup python environment, install dependencies
-# ----------------
+REM ----------------
+REM setup python environment, install dependencies
+REM ----------------
 conda create --n thbenv
 conda activate thbenv
 conda config --env --add channels conda-forge
