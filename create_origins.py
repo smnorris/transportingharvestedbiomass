fb2d532ee1fac10fcdb75b3cab343b2704d1adc9
Simon Norris <snorris@hillcrestgeo.ca>
2021-03-05 17:20:47 -0800

    add arguments to origins script

--- create_origins.py
+++ create_origins.py
@@ -3,32 +3,46 @@ import numpy as np
 import rasterio
 from skimage.measure import label, regionprops_table
 import pandas as pd
+import click
 
-# load image
-with rasterio.open(r"data/00_input.tif") as src:
-    img_source = src.read(1)
-    transform = src.transform
 
-# keep only values greater than 1 and convert to integer
-img_integer = np.where(img_source > 1, img_source, 0).astype(int)
+@click.command()
+@click.argument("in_tif", type=click.Path(exists=True))
+@click.argument("out_csv")
+def create_origins(in_tif, out_csv):
+    # load image
+    with rasterio.open(in_tif) as src:
+        img_source = src.read(1)
+        transform = src.transform
 
-# label - find connected groups of pixels with the same value
-img_label = label(img_integer, connectivity=2)
+    # keep only values greater than 1 and convert to integer
+    img_integer = np.where(img_source > 1, img_source, 0).astype(int)
 
-# find sum of values (in source float array) within each label/group of pixels
-sum_per_label = np.bincount(img_label.flatten(), weights=img_source.flatten())
+    # label - find connected groups of pixels with the same value
+    img_label = label(img_integer, connectivity=2)
 
-# load label ids and centroids into a pandas data frame
-df = pd.DataFrame(regionprops_table(img_label, properties=['centroid']))
+    # find sum of values (in source float array) within each label/group of pixels
+    sum_per_label = np.bincount(img_label.flatten(), weights=img_source.flatten())
 
-# convert the cell references to lat/lon
-xs, ys = rasterio.transform.xy(transform, df["centroid-0"], df["centroid-1"])
-coordpairs = zip(sum_per_label[1:], xs, ys)  # remove the sum for 0 label by stepping up 1
+    # load label ids and centroids into a pandas data frame
+    df = pd.DataFrame(regionprops_table(img_label, properties=["centroid"]))
 
-# dump to csv
-with open(r'data/origins_test.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    # header
-    writer.writerow(["origin_id", "znsums_SUM", "x", "y"])
-    for i, row in enumerate(coordpairs, start=1):
-        writer.writerow([i, row[0], row[1], row[2]])
+    # convert the cell references to lat/lon
+    xs, ys = rasterio.transform.xy(transform, df["centroid-0"], df["centroid-1"])
+    coordpairs = zip(
+        sum_per_label[1:], xs, ys
+    )  # remove the sum for 0 label by stepping up 1
+
+    # dump to csv
+    with open(out_csv, "w", newline="") as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        # header
+        writer.writerow(["origin_id", "znsums_SUM", "x", "y"])
+        for i, row in enumerate(coordpairs, start=1):
+            writer.writerow([i, row[0], row[1], row[2]])
+
+
+if __name__ == "__main__":
+    create_origins()
